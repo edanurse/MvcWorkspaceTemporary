4a242fd69410af134fa3c032d3f1d249289828df
slarlardeveloper <slarlardeveloper@outlook.com>
2022-12-17 17:57:57 +0300

    Add project files.

---
+++ MvcWorkspace/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcWorkspace.Models;
+
+namespace MvcWorkspace.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
---
+++ MvcWorkspace/Models/Expense.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcWorkspace.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        
+        [DisplayName("Expense Name")]
+        [Required]
+        public string ExpenseName { get; set; }
+        
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Amount must be greater than 0!")]
+        public int Amount { get; set; }
+    }
+}
